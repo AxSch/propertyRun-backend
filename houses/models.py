6a89471d612700c610667b232ac0cd9634504e19
AxSch <alex_schuneman@outlook.com>
2024-05-05 21:55:48 +0100

    Add Viewer model

--- houses/models.py
+++ houses/models.py
@@ -6,3 +6,7 @@ class House(models.Model):
     description = models.TextField()
     views = models.PositiveIntegerField(default=0)
     likes = models.PositiveIntegerField(default=0)
+
+class Viewer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
