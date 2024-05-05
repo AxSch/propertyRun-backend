633bae6c8c72dac3fceb0cbf3e91724238ef78b7
AxSch <alex_schuneman@outlook.com>
2024-05-05 21:55:34 +0100

    Add House model

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.CharField(max_length=150)
+    description = models.TextField()
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
