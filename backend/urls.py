bed1e63bea71bc1f9cb654108645fd077ffa88f2
AxSch <alex_schuneman@outlook.com>
2024-05-06 23:33:47 +0100

    Refactor & simplify urls & views

--- backend/urls.py
+++ backend/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.messages import api
 from django.urls import path, include
+from rest_framework import routers
+from houses.views import HouseViewSet, ViewerAPIView
+
+router = routers.DefaultRouter()
+router.register(r'houses', HouseViewSet)
+router.register(r'viewers', ViewerAPIView)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("houses.urls")),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
--- houses/serializers.py
+++ houses/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from houses.models import House
+from houses.models import House, Viewer
 
 class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = House
+        fields = '__all__'
+
+class ViewerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Viewer
         fields = '__all__'
